47845d669fa35590936004d5c96f22c3645007ce
aleksandrayefimova <123464076+aleksandrayefimova@users.noreply.github.com>
2023-10-19 13:33:52 +0200

    online payment page

---
+++ ONLINE_PAYMENT_PAGE-master/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import PaymentForm from "./components/PaymentForm/PaymentForm";
+import TableItem from "./components/TableItem/TableItem";
+import ImageItem from "./components/ImageItem/ImageItem";
+
+function App() {
+    let datas = JSON.parse(localStorage.getItem("data"));
+    const [dataCards, setDataCards] = useState(datas && datas.length>0? datas:[]);
+
+
+    return (
+        <div className="main-container">
+            <a href="https://github.com/valetronika/ONLINE_PAYMENT_PAGE/tree/master" className="link_git">{`github<<< `}</a>
+            <div className="main-container__pay">
+                <PaymentForm dataCards={dataCards} setDataCards={setDataCards} />
+                <ImageItem/>
+            </div>
+            <TableItem dataCards={dataCards} setDataCards={setDataCards}/>
+        </div>
+    );
+}
+
+export default App;
